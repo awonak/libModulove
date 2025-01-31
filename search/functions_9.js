27d6fc32359d3965d97eda3f645174e683ae467f
awonak <adam.wonak@gmail.com>
2025-01-31 06:41:02 +0000

    Deploying to gh-pages from @ awonak/libModulove@f008a58f36bcc9117f686b987a35d5b41dd9cf82 🚀

--- search/functions_9.js
+++ search/functions_9.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['read_0',['Read',['../classmodulove_1_1AnalogInput.html#acada7be45d9b7e12eafbecbceb57296a',1,'modulove::AnalogInput']]],
+  ['read_0',['read',['../classmodulove_1_1AnalogInput.html#acada7be45d9b7e12eafbecbceb57296a',1,'modulove::AnalogInput::Read()'],['../classmodulove_1_1DigitalInput.html#ad075c114fb744c7364314c90ceb5b7d8',1,'modulove::DigitalInput::Read()']]],
   ['rotate_1',['Rotate',['../encoder__dir_8h.html#a8fd106914cc1f33225d4b4f9ea2642da',1,'modulove::arythmatik']]]
 ];
